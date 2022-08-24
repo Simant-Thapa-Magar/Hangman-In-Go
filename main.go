25bdd7a6b2c7e3c982ca59531247de3aae326e62
Simant-Thapa-Magar <tmsimant96@gmail.com>
2022-08-24 15:36:18 +0545

    initialize game with difficulty level selection and word selected accordingly

--- main.go
+++ main.go
@@ -1,17 +1,67 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"math/rand"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+var Scanner = bufio.NewReader(os.Stdin)
+
 func main() {
 	// starting point
+	//hangmanState := 0
+	selectedWord := initializeGame()
+	fmt.Println("Selected word is ", selectedWord)
 }
 
-func initializeGame() {
+func initializeGame() WordDictonary {
 	// initialize game i.e. select a word to start game
+	fmt.Println("Welcome to Hangman")
+
+	validSelection := false
+	var possibleWordOptions []WordDictonary
+	var selectedWord WordDictonary
+
+	for !validSelection {
+		fmt.Printf("Select difficulty mode \n 1 for easy mode \n 2 for medium mode \n 3 for hard mode\n")
+		printReaderIcon()
+		difficultyMode, err := Scanner.ReadString('\n')
+		if err != nil {
+			panic("Something went wrong !! Game terminated !!")
+		}
+
+		difficultyModeInt, err := strconv.Atoi(strings.TrimSpace(difficultyMode))
+
+		if err == nil {
+
+			switch difficultyModeInt {
+			case 1:
+				possibleWordOptions = easyWords
+			case 2:
+				possibleWordOptions = mediumWWords
+			case 3:
+				possibleWordOptions = difficultWords
+			}
+
+			if len(possibleWordOptions) > 0 {
+				validSelection = true
+				selectedWord = possibleWordOptions[generateANumber(len(possibleWordOptions))]
+			}
+		}
+	}
+	return selectedWord
 }
 
 func generateANumber(max int) int {
 	// generates a random number between 0 to max
 	// returns generated random number
-	return max
+	rand.Seed(time.Now().Unix())
+	return rand.Intn(max)
 }
 
 func displayHangman(hangmanState int) {
@@ -40,4 +90,9 @@ func isWordGuessedSuccessfully() {
 
 func isHangmanComplete(hangmanState int) {
 	// determines if hangman has been drawn completely
+}
+
+func printReaderIcon() {
+	// displays a simple greater than icon to indicate user input is expected
+	fmt.Print("> ")
 }
