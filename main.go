1546777f2319c7ecb88b6cb489618c43253eac48
Simant-Thapa-Magar <tmsimant96@gmail.com>
2022-08-24 14:58:52 +0545

    initial skeleton

--- main.go
+++ main.go
@@ -1,11 +1,43 @@
 package main
 
-import (
-	"fmt"
-)
-
 func main() {
-	fmt.Println("easyWords ", easyWords)
-	fmt.Println("mediumWWords ", mediumWWords)
-	fmt.Println("difficultWords ", difficultWords)
+	// starting point
+}
+
+func initializeGame() {
+	// initialize game i.e. select a word to start game
+}
+
+func generateANumber(max int) int {
+	// generates a random number between 0 to max
+	// returns generated random number
+	return max
+}
+
+func displayHangman(hangmanState int) {
+	// displays hangman based on state
+}
+
+func displayGame(targetWord string) {
+	// displays word with guessed and blanks for game
+}
+
+func getUserInput() {
+	// gets input from user for game
+}
+
+func validateUserInput() {
+	// validates user input for game
+}
+
+func determineInputProgress(input string) {
+	// determines if user input is correct or not or if user has asked for hint
+}
+
+func isWordGuessedSuccessfully() {
+	// determines if user has guessed the word successfully
+}
+
+func isHangmanComplete(hangmanState int) {
+	// determines if hangman has been drawn completely
 }
